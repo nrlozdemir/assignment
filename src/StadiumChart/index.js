f09b9eb53b6c4e432250d71b8315d9a6d719ff7e
Nurullah Ozdemir <nrlozdemir@gmail.com>
2019-12-27 03:57:03 +0300

    example chart

--- src/StadiumChart/index.js
+++ src/StadiumChart/index.js
@@ -14,45 +14,30 @@ let stats = [
 ]
 
 const StadiumChart = () => (
-	<div className="">
-		<div className="charts" data-title="Total Percentage">
-      
-      <div className="chart" data={60}>
-				<div className="chart-inner">
-          <svg className="chart-svg" xmlns="http://www.w3.org/2000/svg">
-            <rect 
-              className="chart-bar"
-                  fill="none"
-                  rx="160" x="0" y="0" width="630" height="320"
+	<div className="d-flex justify-space-between align-items-center">
+		<div className="colorList">
+			<div className="colorListItem">Fast</div>
+			<div className="colorListItem">Medium</div>
+			<div className="colorListItem">Slow</div>
+			<div className="colorListItem">Slowest</div>
+		</div>
+		<div className="stadiumCharts" data-title="Total Percentage">
+		{stats.map((stat, index) => {
+      return(
+        <div key={index} className="stadiumChart" data={stat.value}>
+          <div className="tooltip">{[100 - stat.value, '% Slowest'].join('')}</div>
+				  <svg xmlns="http://www.w3.org/2000/svg">
+            <rect className="bar" fill="none"
+              rx={(lineSize * (index + 2))}
+              x={(lineSize / 2)}
+              y={(lineSize / 2)}
+              width={minWidth + (lineSize * (index * 2))}
+              height={minHeight + (lineSize * (index * 2))}
             />
-          </svg>
-        </div>
-			</div>
-      
-      <div className="chart" data={60}>
-				<div className="chart-inner">
-          <svg className="chart-svg" xmlns="http://www.w3.org/2000/svg">
-            <rect 
-              className="chart-bar"
-                  fill="none"
-                  rx="120" x="0" y="0" width="550" height="240"
-            />
-          </svg>
-        </div>
-			</div>
-      
-      <div className="chart" data={60}>
-				<div className="chart-inner">
-          <svg className="chart-svg" xmlns="http://www.w3.org/2000/svg">
-            <rect 
-              className="chart-bar"
-                  fill="none"
-                  rx="80" x="0" y="0" width="470" height="160"
-            />
-          </svg>
-        </div>
-			</div>
-
+				  </svg>
+			  </div>
+      )
+    })}
 		</div>
 	</div>
 )
